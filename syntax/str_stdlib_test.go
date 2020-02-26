dd6f97f66871c09dd9bfbf0d48553289dc82b038
Novan Allanadi <25704935+nofun97@users.noreply.github.com>
2020-02-27 10:38:55 +1100

    add use of AsString in str stdlib and corresponding tests

--- syntax/str_stdlib_test.go
+++ syntax/str_stdlib_test.go
@@ -8,168 +8,83 @@ import (
 
 func TestSub(t *testing.T) {
 	t.Parallel()
-
-	AssertCodesEvalToSameValue(
-		t,
+	AssertCodesEvalToSameValue(t,
 		`"this is a test"`,
-		`//.str.sub("this is not a test", "is not", "is")`,
-	)
-	AssertCodesEvalToSameValue(
-		t,
+		`//.str.sub("this is not a test", "is not", "is")`)
+	AssertCodesEvalToSameValue(t,
+		`"this is a test"`,
+		`//.str.sub("this is not a test", "not ", "")`)
+	AssertCodesEvalToSameValue(t,
 		`"this is still a test"`,
-		`//.str.sub("this is still a test", "doesn't matter", "hello there")`,
-	)
+		`//.str.sub("this is still a test", "doesn't matter", "hello there")`)
 	assertExprPanics(t, `//.str.sub("hello there", "test", 1)`)
 }
 
 func TestSplit(t *testing.T) {
 	t.Parallel()
-
-	AssertCodesEvalToSameValue(
-		t,
-		`["this", "is", "a", "test"]`,
-		`//.str.split("this is a test", " ")`,
-	)
-	AssertCodesEvalToSameValue(
-		t,
-		`["this is a test"]`,
-		`//.str.split("this is a test", ",")`,
-	)
-	AssertCodesEvalToSameValue(
-		t,
-		`["th", " ", " a test"]`,
-		`//.str.split("this is a test", "is")`,
-	)
+	AssertCodesEvalToSameValue(t,
+		`["t", "h", "i", "s", " ", "i", "s", " ", "a", " ", "t", "e", "s", "t"]`,
+		`//.str.split("this is a test", "")`)
+	AssertCodesEvalToSameValue(t, `["this", "is", "a", "test"]`, `//.str.split("this is a test", " ") `)
+	AssertCodesEvalToSameValue(t, `["this is a test"]         `, `//.str.split("this is a test", ",") `)
+	AssertCodesEvalToSameValue(t, `["th", " ", " a test"]     `, `//.str.split("this is a test", "is")`)
 	assertExprPanics(t, `//.str.split("this is a test", 1)`)
 }
 
 func TestLower(t *testing.T) {
 	t.Parallel()
-
-	AssertCodesEvalToSameValue(
-		t,
-		`"this is a test"`,
-		`//.str.lower("THIS IS A TEST")`,
-	)
-	AssertCodesEvalToSameValue(
-		t,
-		`"this is a test"`,
-		`//.str.lower("ThIs is A TeST")`,
-	)
-	AssertCodesEvalToSameValue(
-		t,
-		`"this is a test"`,
-		`//.str.lower("this is a test")`,
-	)
+	AssertCodesEvalToSameValue(t, `""              `, `//.str.lower("")              `)
+	AssertCodesEvalToSameValue(t, `"this is a test"`, `//.str.lower("THIS IS A TEST")`)
+	AssertCodesEvalToSameValue(t, `"this is a test"`, `//.str.lower("ThIs is A TeST")`)
+	AssertCodesEvalToSameValue(t, `"this is a test"`, `//.str.lower("this is a test")`)
 	assertExprPanics(t, `//.str.lower(123)`)
 }
 
 func TestUpper(t *testing.T) {
 	t.Parallel()
-
-	AssertCodesEvalToSameValue(
-		t,
-		`"THIS IS A TEST"`,
-		`//.str.upper("THIS IS A TEST")`,
-	)
-	AssertCodesEvalToSameValue(
-		t,
-		`"THIS IS A TEST"`,
-		`//.str.upper("ThIs is A TeST")`,
-	)
-	AssertCodesEvalToSameValue(
-		t,
-		`"THIS IS A TEST"`,
-		`//.str.upper("this is a test")`,
-	)
+	AssertCodesEvalToSameValue(t, `""              `, `//.str.upper("")              `)
+	AssertCodesEvalToSameValue(t, `"THIS IS A TEST"`, `//.str.upper("THIS IS A TEST")`)
+	AssertCodesEvalToSameValue(t, `"THIS IS A TEST"`, `//.str.upper("ThIs is A TeST")`)
+	AssertCodesEvalToSameValue(t, `"THIS IS A TEST"`, `//.str.upper("this is a test")`)
 	assertExprPanics(t, `//.str.upper(123)`)
 }
 
 func TestTitle(t *testing.T) {
 	t.Parallel()
-
-	AssertCodesEvalToSameValue(
-		t,
-		`"THIS IS A TEST"`,
-		`//.str.title("THIS IS A TEST")`,
-	)
-	AssertCodesEvalToSameValue(
-		t,
-		`"ThIs Is A TeST"`,
-		`//.str.title("ThIs is A TeST")`,
-	)
-	AssertCodesEvalToSameValue(
-		t,
-		`"This Is A Test"`,
-		`//.str.title("this is a test")`,
-	)
+	AssertCodesEvalToSameValue(t, `""              `, `//.str.title("")              `)
+	AssertCodesEvalToSameValue(t, `"THIS IS A TEST"`, `//.str.title("THIS IS A TEST")`)
+	AssertCodesEvalToSameValue(t, `"ThIs Is A TeST"`, `//.str.title("ThIs is A TeST")`)
+	AssertCodesEvalToSameValue(t, `"This Is A Test"`, `//.str.title("this is a test")`)
 	assertExprPanics(t, `//.str.title(123)`)
 }
 
 func TestContains(t *testing.T) {
 	t.Parallel()
-
-	AssertCodesEvalToSameValue(
-		t,
-		`true`,
-		`//.str.contains("this is a test", "is a test")`,
-	)
-	AssertCodesEvalToSameValue(
-		t,
-		`false`,
-		`//.str.contains("this is a test", "is not a test")`,
-	)
+	AssertCodesEvalToSameValue(t, `true `, `//.str.contains("this is a test", "")             `)
+	AssertCodesEvalToSameValue(t, `true `, `//.str.contains("this is a test", "is a test")    `)
+	AssertCodesEvalToSameValue(t, `false`, `//.str.contains("this is a test", "is not a test")`)
 	assertExprPanics(t, `//.str.contains(123, 124)`)
 }
 
 func TestConcat(t *testing.T) {
 	t.Parallel()
-
-	AssertCodesEvalToSameValue(
-		t,
-		`""`,
-		`//.str.concat([])`,
-	)
-	AssertCodesEvalToSameValue(
-		t,
-		`"this is a test"`,
-		`//.str.concat(["this", " is", " a", " test"])`,
-	)
-	AssertCodesEvalToSameValue(
-		t,
-		`"this"`,
-		`//.str.concat(["this"])`,
-	)
+	AssertCodesEvalToSameValue(t, `""              `, `//.str.concat([])                            `)
+	AssertCodesEvalToSameValue(t, `""              `, `//.str.concat(["", "", "", ""])              `)
+	AssertCodesEvalToSameValue(t, `"hello"         `, `//.str.concat(["", "", "", "", "hello", ""]) `)
+	AssertCodesEvalToSameValue(t, `"this is a test"`, `//.str.concat(["this", " is", " a", " test"])`)
+	AssertCodesEvalToSameValue(t, `"this"          `, `//.str.concat(["this"])                      `)
 	assertExprPanics(t, `//.str.concat("this")`)
 }
 
 func TestJoin(t *testing.T) {
 	t.Parallel()
-
-	AssertCodesEvalToSameValue(
-		t,
-		`""`,
-		`//.str.join([], ",")`,
-	)
-	AssertCodesEvalToSameValue(
-		t,
-		`"this is a test"`,
-		`//.str.join(["this", "is", "a", "test"], " ")`,
-	)
-	AssertCodesEvalToSameValue(
-		t,
-		`"this"`,
-		`//.str.join(["this"], ",")`,
-	)
+	AssertCodesEvalToSameValue(t, `""                `, `//.str.join([], ",")                         `)
+	AssertCodesEvalToSameValue(t, `",,"              `, `//.str.join(["", "", ""], ",")               `)
+	AssertCodesEvalToSameValue(t, `"this is a test"  `, `//.str.join(["this", "is", "a", "test"], " ")`)
+	AssertCodesEvalToSameValue(t, `"this"            `, `//.str.join(["this"], ",")                   `)
 	assertExprPanics(t, `//.str.join("this", 2)`)
 }
 
 func assertExprPanics(t *testing.T, code string) {
-	assert.Panics(t, func() {
-		AssertCodesEvalToSameValue(
-			t,
-			`"doesn't matter"`,
-			code,
-		)
-	})
+	assert.Panics(t, func() { AssertCodesEvalToSameValue(t, `"doesn't matter"`, code) })
 }
