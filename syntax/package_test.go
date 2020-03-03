72cd1ea05cbb0bfbce8ba350b2180e528eedc4e4
Chloe <cikeyplanet@gmail.com>
2020-03-03 17:13:14 +1100

    test: add tests for packages

--- syntax/package_test.go
+++ syntax/package_test.go
@@ -8,12 +8,32 @@ import (
 // 	AssertCodesEvalToSameValue(t, `3.141592653589793`, `//.`)
 // }
 
+func TestPackageE(t *testing.T) {
+	AssertCodesEvalToSameValue(t, `2.718281828459045`, `//.math.e`)
+}
+
 func TestPackagePi(t *testing.T) {
 	AssertCodesEvalToSameValue(t, `3.141592653589793`, `//.math.pi`)
 }
 
+// func TestPackageRelativeImport(t *testing.T) {
+// 	AssertCodesEvalToSameValue(t, ``, `//./myutil/work(42)`)
+// }
+
+// func TestPackageYaml(t *testing.T) {
+// 	AssertCodesEvalToSameValue(t, ``, `//./'myutil/work.yaml'`)
+// }
+
+// func TestPackageRootImport(t *testing.T) {
+// 	AssertCodesEvalToSameValue(t, `3.141592653589793`, `///`)
+// }
+
 // func TestPackageExternalImport(t *testing.T) {
 // 	AssertCodesEvalToSameValue(t, `3.141592653589793`, `//github.com/'arr-ai'/arrai/examples/'xml.wbnf'`)
+// }
+
+// func TestPackageExternalImport(t *testing.T) {
+// 	AssertCodesEvalToSameValue(t, `3`, `//http://github.com/'arr-ai'/arrai/examples/'xml.wbnf'`)
 // }
 
 // func TestPackageExternalImport(t *testing.T) {
