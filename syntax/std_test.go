bfa6b35eedc531a523ae0c394019d2ae27cb3997
Marcelo Cantos <marcelo.cantos@gmail.com>
2020-03-27 20:16:12 +1100

    Negative tests for //.tuple and //.dict

--- syntax/std_test.go
+++ syntax/std_test.go
@@ -4,14 +4,22 @@ import "testing"
 
 func TestStdTuple(t *testing.T) {
 	t.Parallel()
+
 	AssertCodesEvalToSameValue(t, `()`, `//.tuple({})`)
 	AssertCodesEvalToSameValue(t, `(a:1)`, `//.tuple({"a":1})`)
 	AssertCodesEvalToSameValue(t, `(a:1, b:2)`, `//.tuple({"a":1, "b":2})`)
+
+	AssertCodePanics(t, `//.tuple((a:1))`)
+	AssertCodePanics(t, `//.tuple(42)`)
 }
 
 func TestStdDict(t *testing.T) {
 	t.Parallel()
+
 	AssertCodesEvalToSameValue(t, `{}`, `//.dict(())`)
 	AssertCodesEvalToSameValue(t, `{"a":1}`, `//.dict((a:1))`)
 	AssertCodesEvalToSameValue(t, `{"a":1, "b":2}`, `//.dict((a:1, b:2))`)
+
+	AssertCodePanics(t, `//.dict({42:43})`)
+	AssertCodePanics(t, `//.dict(42)`)
 }
