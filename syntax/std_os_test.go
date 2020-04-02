413cfc1b3403b9f5557237d1c10133138e839423
Marcelo Cantos <marcelo.cantos@gmail.com>
2020-04-02 21:39:20 +1100

    Improve //.os.stdin test

--- syntax/std_os_test.go
+++ syntax/std_os_test.go
@@ -6,8 +6,13 @@ import (
 )
 
 func TestStdOsStdin(t *testing.T) {
+	// Not parallelisable
 	stdin := stdOsStdinHandle
+	defer func() { stdOsStdinHandle = stdin }()
+
+	stdOsStdinHandle = bytes.NewBuffer([]byte(""))
+	AssertCodesEvalToSameValue(t, `{}`, `//.os.stdin`)
+
 	stdOsStdinHandle = bytes.NewBuffer([]byte("abc"))
 	AssertCodesEvalToSameValue(t, `{|@,@byte| (0,97), (1,98), (2,99)}`, `//.os.stdin`)
-	stdOsStdinHandle = stdin
 }
