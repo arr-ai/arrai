0a745691c61d1238f9728b6a8f27d7392c22f924
Marcelo Cantos <marcelo.cantos@gmail.com>
2020-04-03 13:57:01 +1100

    Test //.encoding.json.decode with Bytes input

--- syntax/std_encoding_json_test.go
+++ syntax/std_encoding_json_test.go
@@ -5,38 +5,41 @@ import "testing"
 func TestJSONDecode(t *testing.T) {
 	t.Parallel()
 	AssertCodePanics(t, `//.encoding.json.decode(123)`)
-	AssertCodesEvalToSameValue(t,
-		`{
-			"a": (s: "string"),
-			"b": 123,
-			"c": 123.321,
-			"d": (a: [1, (s: "string again"), (a: []), {}]),
-			"e": {
-				"f": {
-					"g": (s: "321")
-				},
-				"h": (a: [])
+
+	expected := `{
+		"a": (s: "string"),
+		"b": 123,
+		"c": 123.321,
+		"d": (a: [1, (s: "string again"), (a: []), {}]),
+		"e": {
+			"f": {
+				"g": (s: "321")
 			},
-			"i": (null: {}),
-			"j": (a: [(b: {()}), (b: {})]),
-			"k": (s: {})
-		}`,
-		`//.encoding.json.decode(
-			'{
-				"a": "string",
-				"b": 123,
-				"c": 123.321,
-				"d": [1, "string again", [], {}],
-				"e": {
-					"f": {
-						"g": "321"
-					},
-					"h": []
-				},
-				"i": null,
-				"j": [true, false],
-				"k": ""
-			}'
-		)`,
-	)
+			"h": (a: [])
+		},
+		"i": (null: {}),
+		"j": (a: [(b: {()}), (b: {})]),
+		"k": (s: {})
+	}`
+
+	encoding := `'{
+		"a": "string",
+		"b": 123,
+		"c": 123.321,
+		"d": [1, "string again", [], {}],
+		"e": {
+			"f": {
+				"g": "321"
+			},
+			"h": []
+		},
+		"i": null,
+		"j": [true, false],
+		"k": ""
+	}'`
+
+	// String
+	AssertCodesEvalToSameValue(t, expected, `//.encoding.json.decode(`+encoding+`)`)
+	// Bytes
+	AssertCodesEvalToSameValue(t, expected, `//.encoding.json.decode(//.unicode.utf8.encode(`+encoding+`))`)
 }
