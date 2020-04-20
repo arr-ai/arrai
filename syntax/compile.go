f6af465e3db08efd3e5e146a27f6ddb66bd57f3e
Zhang Eric <Xing.Zhang@anz.com>
2020-04-20 22:46:51 +0800

    #114 Fixed lint issue which error string is started with capitalized letter.

--- syntax/compile.go
+++ syntax/compile.go
@@ -223,7 +223,7 @@ func (pc ParseContext) compileRbinop(b ast.Branch, c ast.Children) rel.Expr {
 
 func (pc ParseContext) compileIf(b ast.Branch, c ast.Children) rel.Expr {
 	log.Error(context.Background(),
-		errors.New("Operator if is deprecated and will be removed soon, please use operator cond instead. "+
+		errors.New("operator if is deprecated and will be removed soon, please use operator cond instead. "+
 			"Operator cond sample: let a = cond ( 2 > 1 : 1, 2 > 3 :2, * : 3)"))
 
 	result := pc.CompileExpr(b.One("expr").(ast.Branch))
