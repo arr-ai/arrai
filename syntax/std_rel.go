ebfe3ded35ab37564aa5dfb5aa9800247cae680a
Novan Allanadi <25704935+nofun97@users.noreply.github.com>
2020-05-04 17:38:31 +1000

    Fix #188, //rel.union can take array of sets or set of sets

--- syntax/std_rel.go
+++ syntax/std_rel.go
@@ -9,7 +9,18 @@ func stdRel() rel.Attr {
 		rel.NewNativeFunctionAttr("union", func(v rel.Value) rel.Value {
 			s := v.(rel.Set)
 			sets := make([]rel.Set, 0, s.Count())
-			for e, ok := s.ArrayEnumerator(); ok && e.MoveNext(); {
+			var e rel.ValueEnumerator
+			switch u := v.(type) {
+			case rel.Array:
+				var ok bool
+				e, ok = u.ArrayEnumerator()
+				if !ok {
+					panic("wat")
+				}
+			case rel.Set:
+				e = u.Enumerator()
+			}
+			for e.MoveNext() {
 				sets = append(sets, e.Current().(rel.Set))
 			}
 			return rel.NUnion(sets...)
