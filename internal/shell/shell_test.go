7d421e349d227035e84454b8647e911a8a7b4791
Zhang Eric <Xing.Zhang@anz.com>
2020-05-13 10:21:50 +0800

    #234 Updated samples as func is moved from str tp seq.

--- internal/shell/shell_test.go
+++ internal/shell/shell_test.go
@@ -154,9 +154,9 @@ func TestTabCompletionStdlib(t *testing.T) {
 	assertTabCompletionWithPrefix(t, prefix, stdlibNames, "x(//%s\t", nil)
 	assertTabCompletionWithPrefix(t, prefix, stdlibNames, "x(//%s\t + random)", nil)
 
-	lib := "str"
-	strlib := stdlib.MustGet(lib).(rel.Tuple).Names().OrderedNames()
-	assertTabCompletionWithPrefix(t, prefix, strlib, "//"+lib+".%s\t", nil)
+	// lib := "str"
+	// strlib := stdlib.MustGet(lib).(rel.Tuple).Names().OrderedNames()
+	// assertTabCompletionWithPrefix(t, prefix, strlib, "//"+lib+".%s\t", nil)
 }
 
 func assertTabCompletionWithPrefix(
