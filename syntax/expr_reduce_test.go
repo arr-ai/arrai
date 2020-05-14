38db58f63377ebab016c5eb697e3ddc47a7e7a3f
Marcelo Cantos <marcelo.cantos@gmail.com>
2020-05-14 22:38:37 +1000

    Fix badly named globals (#264)

--- syntax/expr_reduce_test.go
+++ syntax/expr_reduce_test.go
@@ -4,43 +4,43 @@ import (
 	"testing"
 )
 
-var s = `{|a,b| (3,41), (2,42), (1,43)}`
-var u = `{|a,b| (3,41), (2,42), (1,43), (0, 46)}`
+var testRel1 = `{|a,b| (3,41), (2,42), (1,43)}`
+var testRel2 = `{|a,b| (3,41), (2,42), (1,43), (0, 46)}`
 
 func TestSumExpr(t *testing.T) {
 	t.Parallel()
-	AssertCodesEvalToSameValue(t, `6`, s+` sum .a`)
-	AssertCodesEvalToSameValue(t, `126`, s+` sum .b`)
+	AssertCodesEvalToSameValue(t, `6`, testRel1+` sum .a`)
+	AssertCodesEvalToSameValue(t, `126`, testRel1+` sum .b`)
 }
 
 func TestMaxExpr(t *testing.T) {
 	t.Parallel()
-	AssertCodesEvalToSameValue(t, `3`, s+` max .a`)
-	AssertCodesEvalToSameValue(t, `-1`, s+` max -.a`)
-	AssertCodesEvalToSameValue(t, `43`, s+` max .b`)
+	AssertCodesEvalToSameValue(t, `3`, testRel1+` max .a`)
+	AssertCodesEvalToSameValue(t, `-1`, testRel1+` max -.a`)
+	AssertCodesEvalToSameValue(t, `43`, testRel1+` max .b`)
 }
 
 func TestMeanExpr(t *testing.T) {
 	t.Parallel()
-	AssertCodesEvalToSameValue(t, `2`, s+` mean .a`)
-	AssertCodesEvalToSameValue(t, `-2`, s+` mean -.a`)
-	AssertCodesEvalToSameValue(t, `42`, s+` mean .b`)
-	AssertCodesEvalToSameValue(t, `43`, u+` mean .b`)
+	AssertCodesEvalToSameValue(t, `2`, testRel1+` mean .a`)
+	AssertCodesEvalToSameValue(t, `-2`, testRel1+` mean -.a`)
+	AssertCodesEvalToSameValue(t, `42`, testRel1+` mean .b`)
+	AssertCodesEvalToSameValue(t, `43`, testRel2+` mean .b`)
 }
 
 func TestMinExpr(t *testing.T) {
 	t.Parallel()
-	AssertCodesEvalToSameValue(t, `1`, s+` min .a`)
-	AssertCodesEvalToSameValue(t, `-3`, s+` min -.a`)
-	AssertCodesEvalToSameValue(t, `41`, s+` min .b`)
+	AssertCodesEvalToSameValue(t, `1`, testRel1+` min .a`)
+	AssertCodesEvalToSameValue(t, `-3`, testRel1+` min -.a`)
+	AssertCodesEvalToSameValue(t, `41`, testRel1+` min .b`)
 }
 
 func TestMedianExpr(t *testing.T) {
 	t.Parallel()
-	AssertCodesEvalToSameValue(t, `2`, s+` median .a`)
-	AssertCodesEvalToSameValue(t, `-2`, s+` median -.a`)
-	AssertCodesEvalToSameValue(t, `42`, s+` median .b`)
-	AssertCodesEvalToSameValue(t, `42.5`, u+` median .b`)
-	AssertCodesEvalToSameValue(t, `43`, s+` where .a=1 median .b`)
-	AssertCodesEvalToSameValue(t, `42.5`, s+` where .a<3 median .b`)
+	AssertCodesEvalToSameValue(t, `2`, testRel1+` median .a`)
+	AssertCodesEvalToSameValue(t, `-2`, testRel1+` median -.a`)
+	AssertCodesEvalToSameValue(t, `42`, testRel1+` median .b`)
+	AssertCodesEvalToSameValue(t, `42.5`, testRel2+` median .b`)
+	AssertCodesEvalToSameValue(t, `43`, testRel1+` where .a=1 median .b`)
+	AssertCodesEvalToSameValue(t, `42.5`, testRel1+` where .a<3 median .b`)
 }
