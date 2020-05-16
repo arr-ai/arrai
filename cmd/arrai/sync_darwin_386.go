44ab082d67066083110c98455741338a467ccf12
Marcelo Cantos <cantosm@anz.com>
2020-05-16 22:17:47 +1000

    Avoid broken notify dep on darwin/386

---
+++ cmd/arrai/sync_darwin_386.go
@@ -0,0 +1,14 @@
+// +build: darwin
+// +build: 386
+
+package main
+
+// TODO: Allow watching a single file to represent a non-tuple database.
+
+import (
+	"github.com/urfave/cli/v2"
+)
+
+func sync(c *cli.Context) error {
+	panic("not implemented")
+}
