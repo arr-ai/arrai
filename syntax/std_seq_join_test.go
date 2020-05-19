076afd7d97b0132233c215209c6d45db39bbc99a
Zhang Eric <Xing.Zhang@anz.com>
2020-05-19 16:58:55 +0800

    #234 Removed unnecessary test case.

--- syntax/std_seq_join_test.go
+++ syntax/std_seq_join_test.go
@@ -5,16 +5,16 @@ import "testing"
 func TestStrJoin(t *testing.T) {
 	t.Parallel()
 
-	AssertCodesEvalToSameValue(t, `""                `, `//seq.join(",",[])                         `)
+	AssertCodesEvalToSameValue(t, `""`, `//seq.join(",",[])`)
 
-	AssertCodesEvalToSameValue(t, `"this is a test"  `, `//seq.join(" ",["this", "is", "a", "test"])`)
-	AssertCodesEvalToSameValue(t, `"this"            `, `//seq.join(",",["this"])                   `)
+	AssertCodesEvalToSameValue(t, `"this is a test" `, `//seq.join(" ",["this", "is", "a", "test"])`)
+	AssertCodesEvalToSameValue(t, `"this"`, `//seq.join(",",["this"])`)
 	AssertCodesEvalToSameValue(t, `"You and me"`, `//seq.join(" and ",["You", "me"])`)
-	AssertCodesEvalToSameValue(t, `"AB"`, `//seq.join("",['A','B'])                         `)
+	AssertCodesEvalToSameValue(t, `"AB"`, `//seq.join("",['A','B'])`)
 
 	AssertCodesEvalToSameValue(t, `"Youme"`, `//seq.join("",["You", "me"])`)
-	AssertCodesEvalToSameValue(t, `""                `, `//seq.join(",",[])                         `)
-	AssertCodesEvalToSameValue(t, `",,"              `, `//seq.join(",",["", "", ""])               `)
+	AssertCodesEvalToSameValue(t, `""`, `//seq.join(",",[])`)
+	AssertCodesEvalToSameValue(t, `",,"`, `//seq.join(",",["", "", ""])`)
 
 	// It is not supported
 	// AssertCodesEvalToSameValue(t, `""`, `//seq.join("",[])`)
