e56dbbb57284bf8a006a5428d026774847ebd0cd
Novan Allanadi <25704935+nofun97@users.noreply.github.com>
2020-05-20 11:28:51 +1000

    string test Call and CallAll

--- rel/value_set_str_test.go
+++ rel/value_set_str_test.go
@@ -17,9 +17,39 @@ func TestIsStringTuple(t *testing.T) {
 }
 
 func TestStringCall(t *testing.T) {
+	t.Parallel()
 	s := "hello"
 	f := NewString([]rune(s))
 	for i, c := range s {
 		assert.Equal(t, c, rune(f.Call(NewNumber(float64(i))).(Number).Float64()))
 	}
+
+	assert.Panics(t, func() { f.Call(NewNumber(6)) })
+	assert.Panics(t, func() { f.Call(NewNumber(-1)) })
+}
+
+func TestStringCallAll(t *testing.T) {
+	t.Parallel()
+
+	abc := NewString([]rune("abc"))
+
+	AssertEqualValues(t, NewSet(NewNumber(float64('a'))), abc.CallAll(NewNumber(0)))
+	AssertEqualValues(t, NewSet(NewNumber(float64('b'))), abc.CallAll(NewNumber(1)))
+	AssertEqualValues(t, NewSet(NewNumber(float64('c'))), abc.CallAll(NewNumber(2)))
+	AssertEqualValues(t, None, abc.CallAll(NewNumber(5)))
+	AssertEqualValues(t, None, abc.CallAll(NewNumber(-1)))
+
+	abc = NewOffsetString([]rune("abc"), -2)
+	AssertEqualValues(t, NewSet(NewNumber(float64('a'))), abc.CallAll(NewNumber(-2)))
+	AssertEqualValues(t, NewSet(NewNumber(float64('b'))), abc.CallAll(NewNumber(-1)))
+	AssertEqualValues(t, NewSet(NewNumber(float64('c'))), abc.CallAll(NewNumber(0)))
+	AssertEqualValues(t, None, abc.CallAll(NewNumber(1)))
+	AssertEqualValues(t, None, abc.CallAll(NewNumber(-3)))
+
+	abc = NewOffsetString([]rune("abc"), 2)
+	AssertEqualValues(t, NewSet(NewNumber(float64('a'))), abc.CallAll(NewNumber(2)))
+	AssertEqualValues(t, NewSet(NewNumber(float64('b'))), abc.CallAll(NewNumber(3)))
+	AssertEqualValues(t, NewSet(NewNumber(float64('c'))), abc.CallAll(NewNumber(4)))
+	AssertEqualValues(t, None, abc.CallAll(NewNumber(1)))
+	AssertEqualValues(t, None, abc.CallAll(NewNumber(5)))
 }
