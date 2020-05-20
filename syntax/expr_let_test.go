8fd15a6c9a58a99cc14f95bb33bbb0c41ac1ac9d
Chloe <cikeyplanet@gmail.com>
2020-05-20 16:53:47 +1000

    add test cases

--- syntax/expr_let_test.go
+++ syntax/expr_let_test.go
@@ -65,9 +65,12 @@ func TestExprLetTuplePattern(t *testing.T) {
 }
 
 func TestExprLetDictPattern(t *testing.T) {
+	AssertCodesEvalToSameValue(t, `[4, 5]`, `let d = {"x": 4, "y": 5}; let {"x": a, "y": b} = d; [a, b]`)
 	AssertCodesEvalToSameValue(t, `[4, 5]`, `let {"x": a, "y": b} = {"x": 4, "y": 5}; [a, b]`)
+	AssertCodesEvalToSameValue(t, `4`, `let a = 4; let {"x": (a)} = {"x": 4}; a`)
 	AssertCodePanics(t, `let {"x": a, "y": b} = {"x": 4}; a`)
 	AssertCodePanics(t, `let {"x": a, "y": b} = {"x": 4, "y": 5, "z": 6}; a`)
 	AssertCodePanics(t, `let {"x": a, "x": a} = {"x": 4}; a`)
 	AssertCodePanics(t, `let {"x": a, "x": a} = {"x": 4, "x": 4}; a`)
+	AssertCodePanics(t, `let a = 4; let {"x": (a)} = {"x": 5}; a`)
 }
