001a885878c7a6d639b4d28ec94b7603419261a0
Marcelo Cantos <marcelo.cantos@gmail.com>
2020-05-28 09:43:14 +1000

    //re returns holey arrays for missing submatches (#342)

--- syntax/std_re_test.go
+++ syntax/std_re_test.go
@@ -4,34 +4,33 @@ import "testing"
 
 func TestReMatch(t *testing.T) {
 	AssertCodesEvalToSameValue(t,
-		// TODO: Use n\'abc' syntax when implemented.
-		`let o = \off \s s => (@:.@+off, :.@char);
-		 [['a1', o(1, '1')], [o(2, 'b2'), o(3, '2')], [o(4, 'c3'), o(5, '3')]]`,
+		`[['a1', 1\'1'], [2\'b2', 3\'2'], [4\'c3', 5\'3']]`,
 		"//re.compile(`.(\\d)`).match('a1b2c3')")
 	AssertCodesEvalToSameValue(t,
-		// TODO: Use n\'abc' syntax when implemented.
 		`{}`,
 		"//re.compile(`x(\\d)`).match('a1b2c3')")
 }
 
+func TestReMismatch(t *testing.T) {
+	AssertCodesEvalToSameValue(t,
+		`[['a1'] | 2\[1\'1'], [2\'b~2', 3\'~', 4\'2'], [5\'c3'] | 2\[6\'3']]`,
+		"//re.compile(`[a-z](~)?(\\d)`).match('a1b~2c3')")
+}
+
 func TestReSub(t *testing.T) {
 	AssertCodesEvalToSameValue(t,
-		// TODO: Use n\'abc' syntax when implemented.
 		`'-1-2-3'`,
 		"//re.compile(`.(\\d)`).sub(`-$1`, 'a1b2c3')")
 	AssertCodesEvalToSameValue(t,
-		// TODO: Use n\'abc' syntax when implemented.
 		`'a1b2c3'`,
 		"//re.compile(`x(\\d)`).sub(`-$1`, 'a1b2c3')")
 }
 
 func TestReSubf(t *testing.T) {
 	AssertCodesEvalToSameValue(t,
-		// TODO: Use n\'abc' syntax when implemented.
 		`'A1B2C3'`,
 		"//re.compile(`.(\\d)`).subf(//str.upper, 'a1b2c3')")
 	AssertCodesEvalToSameValue(t,
-		// TODO: Use n\'abc' syntax when implemented.
 		`'a1b2c3'`,
 		"//re.compile(`x(\\d)`).subf(//str.upper, 'a1b2c3')")
 }
