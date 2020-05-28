7c5797938801c2977236e7c0967591a895d21987
Novan Allanadi <25704935+nofun97@users.noreply.github.com>
2020-05-28 10:04:40 +1000

    neater tests

--- syntax/expr_safe_tail_test.go
+++ syntax/expr_safe_tail_test.go
@@ -5,46 +5,26 @@ import "testing"
 func TestSafeTail(t *testing.T) {
 	t.Parallel()
 
-	AssertCodesEvalToSameValue(t, `1 `, `(a: 1).a?:42                                 `)
-	AssertCodesEvalToSameValue(t, `42`, `(a: 1).b?:42                                 `)
-	AssertCodesEvalToSameValue(t, `1 `, `{"a": 1}("a")?:42                            `)
-	AssertCodesEvalToSameValue(t, `42`, `{"a": 1}("b")?:42                            `)
-	AssertCodesEvalToSameValue(t, `1 `, `(a: (b: 1)).a?.b:42                          `)
-	AssertCodesEvalToSameValue(t, `1 `, `{"a": {"b": 1}}("a")?("b"):42                `)
-	AssertCodesEvalToSameValue(t, `1 `, `let a = (b: (c: (d: (e: 1)))); a.b.c?.d.e?:42`)
-	AssertCodesEvalToSameValue(t, `1 `, `let a = (b: (c: (d: (e: 1)))); a.b.c?.d?.e:42`)
-	AssertCodesEvalToSameValue(t, `42`, `let a = (b: (c: (d: (e: 1)))); a.b.c?.d.f?:42`)
-	AssertCodesEvalToSameValue(t, `42`, `let a = (b: (c: (d: (e: 1)))); a.b.c?.f?.e:42`)
-	AssertCodesEvalToSameValue(t,
-		`1`,
-		`let a = {"b": {"c": {"d": {"e": 1}}}}; a("b", "c", "d", "e")?:42            `)
-	AssertCodesEvalToSameValue(t,
-		`42`,
-		`let a = {"b": {"c": {"d": {"e": 1}}}}; a("b", "c", "x", "e")?:42            `)
-	AssertCodesEvalToSameValue(t,
-		`42`,
-		`let a = {"b": {"c": {"d": {"e": 1}}}}; a("b", "c", "x")?("e"):42            `)
-	AssertCodesEvalToSameValue(t,
-		`1`,
-		`let a = {"b": {"c": {"d": {"e": 1}}}}; a("b")("c")?("d")("e")?:42            `)
-	AssertCodesEvalToSameValue(t,
-		`1`,
-		`let a = {"b": {"c": {"d": {"e": 1}}}}; a("b")("c")?("d")?("e"):42            `)
-	AssertCodesEvalToSameValue(t,
-		`42`,
-		`let a = {"b": {"c": {"d": {"e": 1}}}}; a("b")("c")?("d")("f")?:42            `)
-	AssertCodesEvalToSameValue(t,
-		`42`,
-		`let a = {"b": {"c": {"d": {"e": 1}}}}; a("b")("c")?("f")?("e"):42            `)
-	AssertCodesEvalToSameValue(t,
-		`1`,
-		`let a = {"b": (c: (d: {"e": 1}))}; a("b").c?.d("e")?:42                      `)
-	AssertCodesEvalToSameValue(t,
-		`42`,
-		`let a = {"b": (c: (d: {"e": 1}))}; a("b").c?.d("f")?:42                      `)
-	AssertCodesEvalToSameValue(t,
-		`42`,
-		`let a = {"b": (c: (d: {"e": 1}))}; a("b").c?.e?("f")?:42                     `)
+	AssertCodesEvalToSameValue(t, `1 `, `(a: 1).a?:42                                                     `)
+	AssertCodesEvalToSameValue(t, `42`, `(a: 1).b?:42                                                     `)
+	AssertCodesEvalToSameValue(t, `1 `, `{"a": 1}("a")?:42                                                `)
+	AssertCodesEvalToSameValue(t, `42`, `{"a": 1}("b")?:42                                                `)
+	AssertCodesEvalToSameValue(t, `1 `, `(a: (b: 1)).a?.b:42                                              `)
+	AssertCodesEvalToSameValue(t, `1 `, `{"a": {"b": 1}}("a")?("b"):42                                    `)
+	AssertCodesEvalToSameValue(t, `1 `, `let a = (b: (c: (d: (e: 1)))); a.b.c?.d.e?:42                    `)
+	AssertCodesEvalToSameValue(t, `1 `, `let a = (b: (c: (d: (e: 1)))); a.b.c?.d?.e:42                    `)
+	AssertCodesEvalToSameValue(t, `42`, `let a = (b: (c: (d: (e: 1)))); a.b.c?.d.f?:42                    `)
+	AssertCodesEvalToSameValue(t, `42`, `let a = (b: (c: (d: (e: 1)))); a.b.c?.f?.e:42                    `)
+	AssertCodesEvalToSameValue(t, `1 `, `let a = {"b": {"c": {"d": {"e": 1}}}}; a("b", "c", "d", "e")?:42 `)
+	AssertCodesEvalToSameValue(t, `42`, `let a = {"b": {"c": {"d": {"e": 1}}}}; a("b", "c", "x", "e")?:42 `)
+	AssertCodesEvalToSameValue(t, `42`, `let a = {"b": {"c": {"d": {"e": 1}}}}; a("b", "c", "x")?("e"):42 `)
+	AssertCodesEvalToSameValue(t, `1 `, `let a = {"b": {"c": {"d": {"e": 1}}}}; a("b")("c")?("d")("e")?:42`)
+	AssertCodesEvalToSameValue(t, `1 `, `let a = {"b": {"c": {"d": {"e": 1}}}}; a("b")("c")?("d")?("e"):42`)
+	AssertCodesEvalToSameValue(t, `42`, `let a = {"b": {"c": {"d": {"e": 1}}}}; a("b")("c")?("d")("f")?:42`)
+	AssertCodesEvalToSameValue(t, `42`, `let a = {"b": {"c": {"d": {"e": 1}}}}; a("b")("c")?("f")?("e"):42`)
+	AssertCodesEvalToSameValue(t, `1 `, `let a = {"b": (c: (d: {"e": 1}))}; a("b").c?.d("e")?:42          `)
+	AssertCodesEvalToSameValue(t, `42`, `let a = {"b": (c: (d: {"e": 1}))}; a("b").c?.d("f")?:42          `)
+	AssertCodesEvalToSameValue(t, `42`, `let a = {"b": (c: (d: {"e": 1}))}; a("b").c?.e?("f")?:42         `)
 
 	AssertCodeErrors(t, `(a: 1).a?.c:42               `, `(1).c: lhs must be a Tuple, not rel.Number`)
 	AssertCodeErrors(t, `(a: (b: 1)).a?.c:42          `, `Missing attr "c" (available: |b|)`)
