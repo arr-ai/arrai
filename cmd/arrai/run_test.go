fbde73f097f77c1a51069a26e1e04f4e29b87ecc
Zhang Eric <Xing.Zhang@anz.com>
2020-06-03 17:38:42 +0800

    #363 Make error message is more readable.

--- cmd/arrai/run_test.go
+++ cmd/arrai/run_test.go
@@ -13,3 +13,9 @@ func TestEvalFile(t *testing.T) {
 	require.NoError(t, evalFile("../../examples/jsfuncs/jsfuncs.arrai", &buf))
 	require.NoError(t, evalFile("../../examples/grpc/app.arrai", &buf))
 }
+
+func TestEvalNotExistingFile(t *testing.T) {
+	require.Equal(t, `"version": not a command and not found as a file in the current directory`,
+		evalFile("version", nil).Error())
+	require.Equal(t, `"./version": file not found`, evalFile("./version", nil).Error())
+}
