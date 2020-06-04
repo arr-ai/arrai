83aea0101ecede638c04efd19aebbcc8da1b2123
Zhang Eric <Xing.Zhang@anz.com>
2020-06-04 14:04:28 +0800

    #246 Checked in code to add build information.

--- cmd/arrai/version.go
+++ cmd/arrai/version.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/urfave/cli/v2"
 )
@@ -9,11 +10,20 @@ import (
 var versionCommand = &cli.Command{
 	Name:    "version",
 	Aliases: []string{"v"},
-	Usage:   "evaluate the version information of arrai",
+	Usage:   "display arrai version information",
 	Action:  version,
 }
 
 func version(c *cli.Context) error {
-	fmt.Println("version 0.1")
+	fmt.Printf("Build:\n")
+	fmt.Printf("  Version      : %s\n", Version)
+	fmt.Printf("  Git Commit   : %s\n", GitCommit)
+	fmt.Printf("  Date         : %s\n", BuildDate)
+	fmt.Printf("  Go Version   : %s\n", GoVersion)
+	fmt.Printf("  OS           : %s\n", BuildOS)
+	fmt.Printf("Runtime:\n")
+	fmt.Printf("  GOOS/GOARCH  : %s/%s\n", runtime.GOOS, runtime.GOARCH)
+	fmt.Printf("  Go Version   : %s\n", runtime.Version())
+
 	return nil
 }
