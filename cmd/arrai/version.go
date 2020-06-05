951bd943f3a31065cec46ebcc642c3892d2c47b7
Zhang Eric <Xing.Zhang@anz.com>
2020-06-05 17:25:44 +0800

    #246 Added flag --verbose to printed full info or simple info as review comment.

--- cmd/arrai/version.go
+++ cmd/arrai/version.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 
 	"github.com/urfave/cli/v2"
 )
@@ -11,19 +10,26 @@ var versionCommand = &cli.Command{
 	Name:    "version",
 	Aliases: []string{"v"},
 	Usage:   "display arrai version information",
-	Action:  version,
+	Flags: []cli.Flag{
+		&cli.BoolFlag{
+			Name:    "verbose",
+			Aliases: []string{"v"},
+			Usage:   "display full arrai version information",
+		}},
+	Action: version,
 }
 
 func version(c *cli.Context) error {
-	fmt.Printf("Build:\n")
-	fmt.Printf("  Version      : %s\n", Version)
-	fmt.Printf("  Git Commit   : %s\n", GitCommit)
-	fmt.Printf("  Date         : %s\n", BuildDate)
-	fmt.Printf("  Go Version   : %s\n", GoVersion)
-	fmt.Printf("  OS           : %s\n", BuildOS)
-	fmt.Printf("Runtime:\n")
-	fmt.Printf("  GOOS/GOARCH  : %s/%s\n", runtime.GOOS, runtime.GOARCH)
-	fmt.Printf("  Go Version   : %s\n", runtime.Version())
+	if c.Bool("verbose") {
+		fmt.Printf("Version    : %s\n", Version)
+		fmt.Printf("Git commit : %s\n", GitFullCommit)
+		fmt.Printf("Date       : %s\n", BuildDate)
+		fmt.Printf("OS/arch    : %s\n", BuildOS)
+		fmt.Printf("Go version : %s\n", GoVersion)
+	} else {
+		fmt.Printf("Version    : %s\n", Version)
+		fmt.Printf("OS/arch    : %s\n", BuildOS)
+	}
 
 	return nil
 }
