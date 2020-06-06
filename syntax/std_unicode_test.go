bcd320458b225b7f7cdfd57b9ce680aeb6c0caa1
Marcelo Cantos <marcelo.cantos@gmail.com>
2020-06-06 19:36:30 +1000

    Remove unicode library (#409)
    Not required because utf8 conversions are available directly
    via <<string>> and $"${bytes}".

--- syntax/std_unicode_test.go
+++
@@ -1,8 +0,0 @@
-package syntax
-
-import "testing"
-
-func TestStdUnicodeUTF8Encode(t *testing.T) {
-	AssertCodesEvalToSameValue(t, `<<>>          `, `//unicode.utf8.encode("")   `)
-	AssertCodesEvalToSameValue(t, `<<97, 98, 99>>`, `//unicode.utf8.encode("abc")`)
-}
