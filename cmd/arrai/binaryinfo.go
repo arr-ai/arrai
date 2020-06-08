942b955cc41b6f5db90b1fcf64f2357207b72860
Zhang, Eric <59079351+ericzhang6222@users.noreply.github.com>
2020-06-08 13:51:44 +0800

    Add version information for arrai binary (#395)
    Add version information for arrai binary

---
+++ cmd/arrai/binaryinfo.go
@@ -0,0 +1,16 @@
+package main
+
+// Binary info variables are dynamically injected via the `-ldflags` flag with `go build`
+// Version   - Binary version
+// GitFullCommit - Commit SHA of the source code
+// BuildDate - Binary build date
+// GoVersion - Binary build GoVersion
+// BuildOS   - Operating System used to build binary
+//nolint:gochecknoglobals
+var (
+	Version       = "unspecified"
+	GitFullCommit = "unspecified"
+	BuildDate     = "unspecified"
+	GoVersion     = "unspecified"
+	BuildOS       = "unspecified"
+)
