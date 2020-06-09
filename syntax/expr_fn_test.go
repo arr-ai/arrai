99cc77338e84708f7a25ffda26783eedb95bd413
Chloe <cikeyplanet@gmail.com>
2020-06-09 10:23:40 +1000

    fix func name

--- syntax/expr_fn_test.go
+++ syntax/expr_fn_test.go
@@ -2,7 +2,7 @@ package syntax
 
 import "testing"
 
-func TestExprLet(t *testing.T) {
+func TestExprFn(t *testing.T) {
 	t.Parallel()
 	AssertCodesEvalToSameValue(t, `3`, `(\[x, y] x + y)([1, 2])`)
 	AssertCodesEvalToSameValue(t, `3`, `(\z \[x, y] z/(x + y))(9, [1, 2])`)
