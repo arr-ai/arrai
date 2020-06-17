ddb8bc4c7dc1d12b2fe34e731f1235d5f1b7049c
Chloe <cikeyplanet@gmail.com>
2020-06-17 17:02:04 +1000

    add tests

--- syntax/expr_filter_test.go
+++ syntax/expr_filter_test.go
@@ -7,5 +7,6 @@ func TestFilter(t *testing.T) {
 	AssertCodesEvalToSameValue(t, `{42}`, `{1, [2, 3], 4, [5, 6]} filter . {[a, b]: 42}`)
 	AssertCodesEvalToSameValue(t, `{42}`, `{1, [2, 3], 4, [5, 6]} filter . {[_, _]: 42}`)
 	AssertCodesEvalToSameValue(t, `{5, 11}`, `{1, [2, 3], 4, [5, 6]} filter . {[a, b]: a + b}`)
+	AssertCodesEvalToSameValue(t, `{1, 4, 5, 11}`, `{1, [2, 3], 4, [5, 6]} filter . {[a, b]: a + b, k: k}`)
 	AssertCodesEvalToSameValue(t, `{5, 11, 16}`, `{1, [2, 3], 4, [5, 6], [7, 8, 9]} filter . {[a, ..., b]: a + b}`)
 }
