ed23492e9583e87999d809a7991cdfe0eba09ab2
Zhang Eric <Xing.Zhang@anz.com>
2020-07-06 18:39:23 +0800

    Added comments.

--- syntax/std_encoding.go
+++ syntax/std_encoding.go
@@ -16,7 +16,10 @@ func stdEncodingProto() rel.Attr {
 	return rel.NewTupleAttr(
 		"proto",
 		rel.NewAttr("decode", pb.StdProtobufDecoder),
-		// this is a placeholder to represent //encoding.proto.proto
+		// this is a placeholder to represent `//encoding.proto.proto`
+		// Now profobuf decoder method is `//encoding.proto.decode`, and the first argument is `rel.Tuple`.
+		// In order to implement call `//encoding.proto.decode( , //os.file('sysl.pb'))`, so add this placeholder.
+		// The full call can be `//encoding.proto.decode(//encoding.proto.proto , //os.file('sysl.pb'))`.
 		rel.NewAttr("proto", rel.NewTuple()),
 	)
 }
