f9b6d05f84a8420f542dff655a1a32356b9af239
Zhang Eric <Xing.Zhang@anz.com>
2020-07-20 16:00:20 +0800

    Changed test method name to .

--- syntax/std_fmt_test.go
+++ syntax/std_fmt_test.go
@@ -17,8 +17,8 @@ func TestFmtPrettyTuple(t *testing.T) {
 	AssertCodesEvalToSameValue(t, `""`, `//fmt.pretty((a:1,b:2,c:(d:11,e:22)))`)
 }
 
-func TestFmtPrettyMixed(t *testing.T) {
+func TestFmtPrettySet(t *testing.T) {
 	t.Parallel()
-	AssertCodesEvalToSameValue(t, `""`, `//fmt.pretty({'a':1,'b':2,'c':(d:11, e:22)})`)
-	AssertCodesEvalToSameValue(t, `""`, `//fmt.pretty((a:1, b:2, c:{'d':11, 'e':22}))`)
+	AssertCodesEvalToSameValue(t, `""`, `//fmt.pretty({1, 2, 3})`)
+	AssertCodesEvalToSameValue(t, `""`, `//fmt.pretty({1, 2, {3, 4, 5}})`)
 }
