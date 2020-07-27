ea0f629065c93f2d1a33c883662e342935e6f8c5
Zhang Eric <Xing.Zhang@anz.com>
2020-07-27 09:48:31 +0800

    Build info tuple with a coding expression instead of string template.

--- syntax/std_runtime.go
+++ syntax/std_runtime.go
@@ -1,33 +1,11 @@
 package syntax
 
 import (
-	"fmt"
-
 	"github.com/arr-ai/arrai/rel"
 )
 
 // BuildInfo represents arr.ai build information.
 var BuildInfo rel.Value
-
-const buildInfoTemplate = `
-(
-    version: '%s',
-    date: '%s',
-    git: (
-        commit: '%s',
-        tags: ['%s'],
-    ),
-    go: (
-        os: '%s',
-        arch: '%s',
-        compiler: (
-            version: '%s',
-            os: '%s',
-            arch: '%s',
-        ),
-    ),
-)
-`
 
 func stdRuntime() rel.Attr {
 	return rel.NewTupleAttr("arrai",
@@ -37,10 +15,22 @@ func stdRuntime() rel.Attr {
 
 // GetBuildInfo returns arr.ai build information.
 func GetBuildInfo(version, date, fullCommit, tags, os, arch, goVersion string) rel.Value {
-	expr, err := EvaluateExpr(".",
-		fmt.Sprintf(buildInfoTemplate, version, date, fullCommit, tags, os, arch, goVersion, os, arch))
-	if err != nil {
-		return rel.NewString([]rune(err.Error()))
-	}
-	return expr
+	return rel.NewTuple(
+		rel.NewAttr("version", rel.NewString([]rune(version))),
+		rel.NewAttr("date", rel.NewString([]rune(date))),
+		rel.NewAttr("git", rel.NewTuple(
+			rel.NewAttr("commit", rel.NewString([]rune(fullCommit))),
+			// param tags has only one tag now.
+			rel.NewAttr("tags", rel.NewSet(rel.NewString([]rune(tags))))),
+		),
+		rel.NewAttr("go", rel.NewTuple(
+			rel.NewAttr("os", rel.NewString([]rune(os))),
+			rel.NewAttr("arch", rel.NewString([]rune(arch))),
+			rel.NewAttr("compiler", rel.NewTuple(
+				rel.NewAttr("version", rel.NewString([]rune(goVersion))),
+				rel.NewAttr("os", rel.NewString([]rune(os))),
+				rel.NewAttr("arch", rel.NewString([]rune(arch)))),
+			)),
+		),
+	)
 }
--- syntax/std_runtime_test.go
+++ syntax/std_runtime_test.go
@@ -12,7 +12,7 @@ func TestGetBuildInfo(t *testing.T) {
 		"DIRTY-v0.99.0", "2020-07-23T10:40:08Z", "d399e13f3670c6698ba35148e6f545322e20e1fb",
 		"v0.99.0", "darwin", "amd64", "go1.14 darwin/amd64"))
 	assert.Equal(t, `(date: '2020-07-23T10:40:08Z', `+
-		`git: (commit: 'd399e13f3670c6698ba35148e6f545322e20e1fb', tags: ['v0.99.0']), `+
+		`git: (commit: 'd399e13f3670c6698ba35148e6f545322e20e1fb', tags: {'v0.99.0'}), `+
 		`go: (arch: 'amd64', compiler: (arch: 'amd64', os: 'darwin', version: 'go1.14 darwin/amd64'), os: 'darwin'), `+
 		`version: 'DIRTY-v0.99.0')`, str)
 }
