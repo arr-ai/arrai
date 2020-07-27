abed3b5f3865af2274468c74bd75e514b69d55dd
Zhang Eric <Xing.Zhang@anz.com>
2020-07-27 09:56:36 +0800

    Updated test case to one code expression.

--- syntax/std_runtime_test.go
+++ syntax/std_runtime_test.go
@@ -3,6 +3,7 @@ package syntax
 import (
 	"testing"
 
+	"github.com/arr-ai/arrai/rel"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,9 +11,9 @@ func TestGetBuildInfo(t *testing.T) {
 	assert.Equal(t, `(date: '2020-07-23T10:40:08Z', `+
 		`git: (commit: 'd399e13f3670c6698ba35148e6f545322e20e1fb', tags: {'v0.99.0'}), `+
 		`go: (arch: 'amd64', compiler: (arch: 'amd64', os: 'darwin', version: 'go1.14 darwin/amd64'), os: 'darwin'), `+
-		`version: 'DIRTY-v0.99.0')`, 
+		`version: 'DIRTY-v0.99.0')`,
 		rel.Repr(GetBuildInfo(
 			"DIRTY-v0.99.0", "2020-07-23T10:40:08Z", "d399e13f3670c6698ba35148e6f545322e20e1fb",
-			"v0.99.0", "darwin", "amd64", "go1.14 darwin/amd64"))
-		)
+			"v0.99.0", "darwin", "amd64", "go1.14 darwin/amd64")),
+	)
 }
