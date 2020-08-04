17ef1c76dcb459478dd199b26145c4d614912569
Oliver Lade <oliver.lade@anz.com>
2020-08-04 13:24:02 +1000

    Fix pretty printing tests

--- syntax/std_fmt_test.go
+++ syntax/std_fmt_test.go
@@ -7,19 +7,71 @@ import (
 func TestFmtPrettyDict(t *testing.T) {
 	t.Parallel()
 
-	AssertCodesEvalToSameValue(t, `{'a': 1, 'b': 2, 'c': 3}`, `//fmt.pretty({'b':2, 'a':1,'c':3})`)
-	AssertCodesEvalToSameValue(t, `{'a': 1, 'b': 2, 'c': {'d': 11, 'e': 22}}`,
-		`//fmt.pretty({'b':2,'a':1,'c':{'d':11,'e':22}})`)
+	AssertCodesEvalToSameValue(t, `"{
+  'a': 1,
+  'b': 2,
+  'c': 3
+}"`, `//fmt.pretty({'b':2, 'a':1,'c':3})`)
+
+	AssertCodesEvalToSameValue(t, `"{
+  'a': 1,
+  'b': 2,
+  'c': {
+    'd': 11,
+    'e': 22
+  }
+}"`, `//fmt.pretty({'b':2,'a':1,'c':{'d':11,'e':22}})`)
 }
 
 func TestFmtPrettyTuple(t *testing.T) {
 	t.Parallel()
-	AssertCodesEvalToSameValue(t, `(a:1,b:2,c:3)`, `//fmt.pretty((a:1,b:2,c:3))`)
-	AssertCodesEvalToSameValue(t, `(a:1,b:2,c:(d:11,e:22))`, `//fmt.pretty((a:1,b:2,c:(d:11,e:22)))`)
+	AssertCodesEvalToSameValue(t, `"(
+  a: 1,
+  b: 2,
+  c: 3
+)"`, `//fmt.pretty((a:1,c:3,b:2))`)
+	AssertCodesEvalToSameValue(t, `"(
+  a: 1,
+  b: 2,
+  c: (
+    d: 11,
+    e: 22
+  )
+)"`, `//fmt.pretty((a:1,b:2,c:(d:11,e:22)))`)
+}
+
+func TestFmtPrettyArray(t *testing.T) {
+	t.Parallel()
+	AssertCodesEvalToSameValue(t, `"[
+  1,
+  2,
+  3
+]"`, `//fmt.pretty([1, 2, 3])`)
+	AssertCodesEvalToSameValue(t, `"[
+  1,
+  2,
+  [
+    3,
+    4,
+    5
+  ]
+]"`, `//fmt.pretty([1, 2, [3, 4, 5]])`)
 }
 
 func TestFmtPrettySet(t *testing.T) {
 	t.Parallel()
-	AssertCodesEvalToSameValue(t, `{1, 2, 3}`, `//fmt.pretty({1, 2, 3})`)
-	AssertCodesEvalToSameValue(t, `{1, 2, {3, 4, 5}}`, `//fmt.pretty({1, 2, {3, 4, 5}})`)
+	AssertCodesEvalToSameValue(t, `"{
+  1,
+  2,
+  3
+}"`, `//fmt.pretty({1, 2, 3})`)
+	AssertCodesEvalToSameValue(t, `"{
+  1,
+  2,
+  {
+    3,
+    4,
+    5
+  }
+}"`, `//fmt.pretty({1, 2, {3, 4, 5}})`)
 }
