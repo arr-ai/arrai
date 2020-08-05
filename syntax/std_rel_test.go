bcebc1f328a9e0c4566a92ff510c577e3d243668
Oliver <Oliver.Lade@anz.com>
2020-08-05 13:09:26 +1000

    Log errors when asserts fail (#538)
    * Log errors when asserts fail
    
    * Fix TestRelUnion

--- syntax/std_rel_test.go
+++ syntax/std_rel_test.go
@@ -5,8 +5,8 @@ import "testing"
 func TestRelUnion(t *testing.T) {
 	t.Parallel()
 
-	AssertCodeEvalsToType(t, `{1, 2, 3, 4}`, `//rel.union({{1, 2}, {2, 3}, {3, 4}})`)
-	AssertCodeEvalsToType(t, `{1, 2, 3}   `, `//rel.union({{1}, {2}, {3}})         `)
-	AssertCodeEvalsToType(t, `{1}         `, `//rel.union({{1}, {1}, {1}})         `)
-	AssertCodeEvalsToType(t, `{}          `, `//rel.union({})                      `)
+	AssertCodesEvalToSameValue(t, `{1, 2, 3, 4}`, `//rel.union({{1, 2}, {2, 3}, {3, 4}})`)
+	AssertCodesEvalToSameValue(t, `{1, 2, 3}   `, `//rel.union({{1}, {2}, {3}})         `)
+	AssertCodesEvalToSameValue(t, `{1}         `, `//rel.union({{1}, {1}, {1}})         `)
+	AssertCodesEvalToSameValue(t, `{}          `, `//rel.union({})                      `)
 }
