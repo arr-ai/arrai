f3ad61dc7ad24f0f8f5e697ab5db5a163d4c443d
Novan <25704935+nofun97@users.noreply.github.com>
2020-08-09 17:31:52 +1000

    dot base test

--- syntax/expr_tuple_project_test.go
+++ syntax/expr_tuple_project_test.go
@@ -11,6 +11,13 @@ func TestTupleProjectExpr(t *testing.T) {
 	AssertCodesEvalToSameValue(t, `(b: 2, c: 3)      `, `(a: 1, b: 2, c: 3).~|a|      `)
 	AssertCodesEvalToSameValue(t, `(c: 3)            `, `(a: 1, b: 2, c: 3).~|a, b|   `)
 	AssertCodesEvalToSameValue(t, `()                `, `(a: 1, b: 2, c: 3).~|a, b, c|`)
+
+	AssertCodesEvalToSameValue(t, `(a: 1)            `, `(a: 1, b: 2, c: 3) -> .|a|       `)
+	AssertCodesEvalToSameValue(t, `(a: 1, b: 2)      `, `(a: 1, b: 2, c: 3) -> .|a, b|    `)
+	AssertCodesEvalToSameValue(t, `(a: 1, b: 2, c: 3)`, `(a: 1, b: 2, c: 3) -> .|a, b, c| `)
+	AssertCodesEvalToSameValue(t, `(b: 2, c: 3)      `, `(a: 1, b: 2, c: 3) -> .~|a|      `)
+	AssertCodesEvalToSameValue(t, `(c: 3)            `, `(a: 1, b: 2, c: 3) -> .~|a, b|   `)
+	AssertCodesEvalToSameValue(t, `()                `, `(a: 1, b: 2, c: 3) -> .~|a, b, c|`)
 }
 
 func TestTupleProjectExprError(t *testing.T) {
@@ -24,4 +31,13 @@ func TestTupleProjectExprError(t *testing.T) {
 	AssertCodeErrors(t, `names are not subset of lhs: |a, b, c|`, `(a: 1, b: 2, c: 3).~|d|         `)
 	AssertCodeErrors(t, `names are not subset of lhs: |a, b, c|`, `(a: 1, b: 2, c: 3).~|a, b, d|   `)
 	AssertCodeErrors(t, `names are not subset of lhs: |a, b, c|`, `(a: 1, b: 2, c: 3).~|a, b, c, d|`)
+
+	AssertCodeErrors(t, `lhs does not evaluate to tuple: {1: 1}`, `{1: 1} -> .|a|                      `)
+	AssertCodeErrors(t, `lhs does not evaluate to tuple: {1: 1}`, `{1: 1} -> .~|a|                     `)
+	AssertCodeErrors(t, `names are not subset of lhs: |a, b, c|`, `(a: 1, b: 2, c: 3) -> .|d|          `)
+	AssertCodeErrors(t, `names are not subset of lhs: |a, b, c|`, `(a: 1, b: 2, c: 3) -> .|a, b, d|    `)
+	AssertCodeErrors(t, `names are not subset of lhs: |a, b, c|`, `(a: 1, b: 2, c: 3) -> .|a, b, c, d| `)
+	AssertCodeErrors(t, `names are not subset of lhs: |a, b, c|`, `(a: 1, b: 2, c: 3) -> .~|d|         `)
+	AssertCodeErrors(t, `names are not subset of lhs: |a, b, c|`, `(a: 1, b: 2, c: 3) -> .~|a, b, d|   `)
+	AssertCodeErrors(t, `names are not subset of lhs: |a, b, c|`, `(a: 1, b: 2, c: 3) -> .~|a, b, c, d|`)
 }
