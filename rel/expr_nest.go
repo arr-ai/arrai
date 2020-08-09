5e39d331f9a4a6d577c33b08fad3b6798064a2c3
Novan Allanadi <25704935+nofun97@users.noreply.github.com>
2020-08-10 08:33:08 +1000

    Implement attribute-set complement in nest (#560)

--- rel/expr_nest.go
+++ rel/expr_nest.go
@@ -10,14 +10,15 @@ import (
 // NestExpr returns the relation with names grouped into a nested relation.
 type NestExpr struct {
 	ExprScanner
-	lhs   Expr
-	attrs Names
-	attr  string
+	inverse bool
+	lhs     Expr
+	attrs   Names
+	attr    string
 }
 
 // NewNestExpr returns a new NestExpr.
-func NewNestExpr(scanner parser.Scanner, lhs Expr, attrs Names, attr string) Expr {
-	return &NestExpr{ExprScanner{scanner}, lhs, attrs, attr}
+func NewNestExpr(scanner parser.Scanner, inverse bool, lhs Expr, attrs Names, attr string) Expr {
+	return &NestExpr{ExprScanner{scanner}, inverse, lhs, attrs, attr}
 }
 
 // String returns a string representation of the expression.
@@ -32,6 +33,19 @@ func (e *NestExpr) Eval(local Scope) (Value, error) {
 		return nil, WrapContext(err, e, local)
 	}
 	if set, ok := value.(Set); ok {
+		if e.inverse {
+			setRelAttrs := mustGetRelationAttrs(set)
+			if err := validNestOp(setRelAttrs, e.attrs); err != nil {
+				return nil, WrapContext(err, e, local)
+			}
+			e.attrs = setRelAttrs.Minus(e.attrs)
+			if !e.attrs.IsTrue() {
+				return nil, WrapContext(
+					fmt.Errorf("nest attrs cannot be on all of relation attrs (%v)", setRelAttrs),
+					e, local,
+				)
+			}
+		}
 		return Nest(set, e.attrs, e.attr), nil
 	}
 	return nil, WrapContext(errors.Errorf("nest not applicable to %T", value), e, local)
