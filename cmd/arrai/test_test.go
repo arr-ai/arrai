cfbbf796fa1d2f502c4b803476210a9b06f6d337
ericzhang6222 <lordofpm@163.com>
2020-08-24 00:35:03 +0800

    Updated test case to support windows OS.

--- cmd/arrai/test_test.go
+++ cmd/arrai/test_test.go
@@ -13,11 +13,17 @@ func TestTest(t *testing.T) {
 	var s strings.Builder
 	err := testPath("./../../examples/test", &s)
 	require.Nil(t, err)
-	require.Equal(t,
-		`Tests:
+	windowsOsStr := `Tests:
+..\..\examples\test\multiple_cases_test.arrai
+..\..\examples\test\single_case_test.arrai
+2/2 tests passed
+all tests passed
+`
+	linuxOsStr := `Tests:
 ../../examples/test/multiple_cases_test.arrai
 ../../examples/test/single_case_test.arrai
 2/2 tests passed
 all tests passed
-`, s.String())
+`
+	require.True(t, (windowsOsStr == s.String()) || (linuxOsStr == s.String()))
 }
