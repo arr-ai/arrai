arr-ai/arrai

commit de505ee0aca95e5477bea2e4a9f82c20b9800548

Dynamic vars (#612)
* Dynamic variables

rel/pattern_set.go
@@ -22,37 +22,25 @@ func NewSetPattern(patterns ...Pattern) SetPattern {
 	return SetPattern{patterns}
 }
 
-func (p SetPattern) Bind(ctx context.Context, local Scope, value Value) (Scope, error) {
-	set, is := value.(GenericSet)
-	if !is {
-		return EmptyScope, fmt.Errorf("value %s is not a set", value)
-	}
-
+func (p SetPattern) Bind(ctx context.Context, local Scope, value Value) (context.Context, Scope, error) {
+	set := value.(Set)
 	extraElements := make(map[int]int)
 	for i, ptn := range p.patterns {
-		if _, is := ptn.(ExtraElementPattern); is {
+		switch ptn.(type) {
+		case ExtraElementPattern, IdentPattern, DynIdentPattern:
 			if len(extraElements) == 1 {
-				return EmptyScope, fmt.Errorf("non-deterministic pattern is not supported yet")
+				return ctx, EmptyScope, fmt.Errorf("non-deterministic pattern is not supported yet")
 			}
 			extraElements[i] = set.Count() - len(p.patterns)
-			continue
-		}
-		if t, is := ptn.(ExprPattern); is {
-			if _, is = t.Expr.(IdentExpr); is {
-				if len(extraElements) == 1 {
-					return EmptyScope, fmt.Errorf("non-deterministic pattern is not supported yet")
-				}
-				extraElements[i] = set.Count() - len(p.patterns)
-			}
 		}
 	}
 
 	if len(p.patterns) > set.Count()+len(extraElements) {
-		return EmptyScope, fmt.Errorf("length of set %s shorter than set pattern %s", set, p)
+		return ctx, EmptyScope, fmt.Errorf("length of set %s shorter than set pattern %s", set, p)
 	}
 
 	if len(extraElements) == 0 && len(p.patterns) < set.Count() {
-		return EmptyScope, fmt.Errorf("length of set %s longer than set pattern %s", set, p)
+		return ctx, EmptyScope, fmt.Errorf("length of set %s longer than set pattern %s", set, p)
 	}
 
 	result := EmptyScope
@@ -61,18 +49,19 @@ func (p SetPattern) Bind(ctx context.Context, local Scope, value Value) (Scope, 
 			continue
 		}
 		switch t := ptn.(type) {
+		case IdentPattern:
+		case DynIdentPattern:
 		case ExprPattern:
-			v, is := t.Expr.(Value)
-			if is {
+			if v, is := t.Expr.(Value); is {
 				if !set.Has(v) {
-					return EmptyScope, fmt.Errorf("item %s is not included in set %s", v, value)
+					return ctx, EmptyScope, fmt.Errorf("item %s is not included in set %s", v, value)
 				}
 				set = set.Without(v).(GenericSet)
 				continue
 			}
 
 			if _, is := t.Expr.(IdentExpr); !is {
-				return EmptyScope, fmt.Errorf("item type %s is not supported yet", t)
+				return ctx, EmptyScope, fmt.Errorf("item type %s is not supported yet", t)
 			}
 		case ExprsPattern:
 			// Support cases:
@@ -81,43 +70,47 @@ func (p SetPattern) Bind(ctx context.Context, local Scope, value Value) (Scope, 
 			if identExpr, is := t.exprs[0].(IdentExpr); is {
 				v, has := local.Get(identExpr.ident)
 				if !has {
-					return EmptyScope, fmt.Errorf("%q not in scope", identExpr.ident)
+					return ctx, EmptyScope, fmt.Errorf("%q not in scope", identExpr.ident)
 				}
 				if !set.Has(v.(Value)) {
-					return EmptyScope, fmt.Errorf("item %s is not included in set %s", v, value)
+					return ctx, EmptyScope, fmt.Errorf("item %s is not included in set %s", v, value)
 				}
 				set = set.Without(v.(Value)).(GenericSet)
 			}
 		default:
-			return EmptyScope, fmt.Errorf("%s not supported yet", t)
+			panic(fmt.Errorf("pattern typt %T not supported yet", t))
 		}
 	}
 	for i := range extraElements {
 		var scope Scope
 		var err error
 		if _, is := p.patterns[i].(ExtraElementPattern); is {
-			scope, err = p.patterns[i].Bind(ctx, local, set)
+			ctx, scope, err = p.patterns[i].Bind(ctx, local, set)
 		} else {
 			if set.Count() != 1 {
-				return EmptyScope, fmt.Errorf("the length of set %s is wrong", set)
+				return ctx, EmptyScope, fmt.Errorf("the length of set %s is wrong", set)
 			}
 
-			scope, err = p.patterns[i].Bind(ctx, local, set.set.Any().(Value))
+			e := set.Enumerator()
+			if !e.MoveNext() {
+				panic("set with count 1 failed to enumerate")
+			}
+			ctx, scope, err = p.patterns[i].Bind(ctx, local, e.Current())
 			if err != nil {
-				return EmptyScope, err
+				return ctx, EmptyScope, err
 			}
 		}
 		if err != nil {
-			return EmptyScope, err
+			return ctx, EmptyScope, err
 		}
 
 		result, err = result.MatchedUpdate(scope)
 		if err != nil {
-			return EmptyScope, err
+			return ctx, EmptyScope, err
 		}
 	}
 
-	return result, nil
+	return ctx, result, nil
 }
 
 func (p SetPattern) String() string {

rel/expr_arrow.go
@@ -33,7 +33,7 @@ func (e *ArrowExpr) Eval(ctx context.Context, local Scope) (_ Value, err error) 
 	if err != nil {
 		return nil, WrapContextErr(err, e, local)
 	}
-	scope, err := e.fn.arg.Bind(ctx, local, value)
+	ctx, scope, err := e.fn.arg.Bind(ctx, local, value)
 	if err != nil {
 		return nil, WrapContextErr(err, e, local)
 	}

rel/expr_cond_pattern_control_var.go
@@ -50,7 +50,7 @@ func (expr CondPatternControlVarExpr) Eval(ctx context.Context, scope Scope) (Va
 	}
 
 	for _, conditionPair := range expr.conditionPairs {
-		bindings, err := conditionPair.Bind(ctx, scope, varVal)
+		ctx, bindings, err := conditionPair.Bind(ctx, scope, varVal)
 		if err == nil {
 			l, err := scope.MatchedUpdate(bindings)
 			if err != nil {

rel/expr_darrow.go
@@ -34,7 +34,9 @@ func (e *DArrowExpr) Eval(ctx context.Context, local Scope) (_ Value, err error)
 	if set, ok := value.(Set); ok {
 		values := []Value{}
 		for i := set.Enumerator(); i.MoveNext(); {
-			scope, err := e.fn.arg.Bind(ctx, local, i.Current())
+			var scope Scope
+			var err error
+			ctx, scope, err = e.fn.arg.Bind(ctx, local, i.Current())
 			if err != nil {
 				return nil, WrapContextErr(err, e, local)
 			}

rel/expr_ident.go
@@ -14,11 +14,14 @@ type IdentExpr struct {
 }
 
 // NewIdentExpr returns a new identifier.
-func NewIdentExpr(scanner parser.Scanner, ident string) IdentExpr {
+func NewIdentExpr(scanner parser.Scanner, ident string) Expr {
+	if isDynIdent(ident) {
+		return DynIdentExpr{IdentExpr: IdentExpr{ExprScanner{scanner}, ident}}
+	}
 	return IdentExpr{ExprScanner{scanner}, ident}
 }
 
-func NewDotIdent(source parser.Scanner) IdentExpr {
+func NewDotIdent(source parser.Scanner) Expr {
 	return NewIdentExpr(source, ".")
 }
 
@@ -41,4 +44,16 @@ func (e IdentExpr) Eval(ctx context.Context, local Scope) (Value, error) {
 		return a.Eval(ctx, local)
 	}
 	return nil, WrapContextErr(fmt.Errorf("name %q not found in %v", e.ident, local.m.Keys()), e, local)
+}
+
+type DynIdentExpr struct {
+	IdentExpr
+}
+
+// Eval returns the value from scope corresponding to the ident.
+func (e DynIdentExpr) Eval(ctx context.Context, local Scope) (Value, error) {
+	if a := ctx.Value(DynIdent(e.ident)); a != nil {
+		return a.(Value), nil
+	}
+	return nil, WrapContextErr(fmt.Errorf("dynamic variable %s not found", e.ident), e, local)
 }

rel/expr_recursion.go
@@ -10,13 +10,13 @@ import (
 
 type RecursionExpr struct {
 	ExprScanner
-	name      ExprPattern
+	name      Pattern
 	fn        Expr
 	fix, fixt Value
 }
 
-func NewRecursionExpr(scanner parser.Scanner, name Expr, fn Expr, fix, fixt Value) Expr {
-	return RecursionExpr{ExprScanner{scanner}, NewExprPattern(name), fn, fix, fixt}
+func NewRecursionExpr(scanner parser.Scanner, name string, fn Expr, fix, fixt Value) Expr {
+	return RecursionExpr{ExprScanner{scanner}, NewIdentPattern(name), fn, fix, fixt}
 }
 
 func (r RecursionExpr) Eval(ctx context.Context, local Scope) (Value, error) {

rel/expr_tuplemap.go
@@ -31,7 +31,7 @@ func (e *TupleMapExpr) Eval(ctx context.Context, local Scope) (Value, error) {
 		return nil, WrapContextErr(err, e, local)
 	}
 	return value.(Tuple).Map(func(v Value) (Value, error) {
-		scope, err := e.fn.arg.Bind(ctx, local, v) //nolint: errcheck
+		ctx, scope, err := e.fn.arg.Bind(ctx, local, v)
 		if err != nil {
 			return nil, err
 		}

rel/pattern.go
@@ -11,7 +11,7 @@ type Pattern interface {
 	fmt.Stringer
 
 	// Bind binds pattern with value and add the binding pair to scope
-	Bind(ctx context.Context, scope Scope, value Value) (Scope, error)
+	Bind(ctx context.Context, scope Scope, value Value) (context.Context, Scope, error)
 
 	// Bindings returns all the names a pattern expects to bind
 	Bindings() []string

commit d5fd60499df064bd43cbc100c5c4c680c911be25

Fix bug introduced by #612

rel/pattern_set.go
@@ -23,7 +23,10 @@ func NewSetPattern(patterns ...Pattern) SetPattern {
 }
 
 func (p SetPattern) Bind(ctx context.Context, local Scope, value Value) (context.Context, Scope, error) {
-	set := value.(Set)
+	set, is := value.(Set)
+	if !is {
+		return ctx, EmptyScope, fmt.Errorf("value %s is not a set", value)
+	}
 	extraElements := make(map[int]int)
 	for i, ptn := range p.patterns {
 		switch ptn.(type) {