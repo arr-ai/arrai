3fb477c10538cee1cda993651a3568fc508e710d
Novan Allanadi <25704935+nofun97@users.noreply.github.com>
2020-09-03 21:24:33 +1000

    added tests

--- cmd/arrai/run_test.go
+++ cmd/arrai/run_test.go
@@ -5,18 +5,23 @@ import (
 	"context"
 	"os"
 	"path/filepath"
+	s "sync"
 	"testing"
 
 	"github.com/arr-ai/arrai/pkg/arraictx"
 	"github.com/arr-ai/arrai/pkg/ctxfs"
 	"github.com/arr-ai/arrai/syntax"
 	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestModuleImportRoot(t *testing.T) {
-	t.Parallel()
+var (
+	loadImportFs s.Once
+	importFs     afero.Fs
+)
 
+func getImportFs(t *testing.T) afero.Fs {
 	//
 	// path1
 	// ├── ModuleRootSentinel
@@ -31,38 +36,94 @@ func TestModuleImportRoot(t *testing.T) {
 	//             ├── 2.arrai
 	//             └── import_from_nested_root.arrai
 	//
-	importTestFs := afero.NewMemMapFs()
-	require.NoError(t, importTestFs.MkdirAll(mustAbs(t, "path1/path2/path3"), os.ModeDir))
-	require.NoError(t, importTestFs.MkdirAll(mustAbs(t, "path1/path2/path4"), os.ModeDir))
-	files := []struct {
-		fileName, expr string
-	}{
-		{filepath.Join("path1", syntax.ModuleRootSentinel), ""},
-		{filepath.Join("path1/path2/path4", syntax.ModuleRootSentinel), ""},
-		{"path1/path2/path3/import_from_root.arrai", "//{/path2/path4/path5/2}"},
-		{"path1/path2/path4/1.arrai", "1"},
-		{"path1/path2/path4/import_from_same_dir_root.arrai", "//{/1}"},
-		{"path1/path2/path4/path5/2.arrai", "2"},
-		{"path1/path2/path4/path5/import_from_nested_root.arrai", "//{/1}"},
-	}
-	for _, af := range files {
-		f, err := importTestFs.Create(mustAbs(t, af.fileName))
-		require.NoError(t, err)
-		defer f.Close()
-		mustWrite(t, f, []byte(af.expr))
-	}
+	loadImportFs.Do(func() {
+		importFs = afero.NewMemMapFs()
+		require.NoError(t, importFs.MkdirAll("/path1/path2/path3", os.ModeDir))
+		require.NoError(t, importFs.MkdirAll("/path1/path2/path4", os.ModeDir))
+		files := []struct {
+			fileName, expr string
+		}{
+			{
+				filepath.Join("/path1", syntax.ModuleRootSentinel),
+				"module github.com/test/path1\n",
+			},
+			{
+				filepath.Join("/path1/path2/path4", syntax.ModuleRootSentinel),
+				"module github.com/test/path1/path2/path4\n",
+			},
+			{"/path1/path2/path3/import_from_root.arrai", "//{/path2/path4/path5/2}"},
+			{"/path1/path2/path4/1.arrai", "1"},
+			{"/path1/path2/path4/import_from_same_dir_root.arrai", "//{/1}"},
+			{"/path1/path2/path4/path5/2.arrai", "2"},
+			{"/path1/path2/path4/path5/import_from_nested_root.arrai", "//{/1}"},
+		}
+		for _, af := range files {
+			f, err := importFs.Create(mustAbs(t, af.fileName))
+			require.NoError(t, err)
+			defer f.Close()
+			mustWrite(t, f, []byte(af.expr))
+		}
+	})
+	return importFs
+}
 
+func TestRunBundle(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name, filePath, result string
+	}{
+		{"single file", "/path1/path2/path4/1.arrai", "1"},
+		{"single root", "/path1/path2/path4/path5/import_from_nested_root.arrai", "1"},
+		{"nested roots", "/path1/path2/path3/import_from_root.arrai", "2"},
+	}
+	ctx := ctxfs.SourceFsOnto(context.Background(), getImportFs(t))
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			zipped := &bytes.Buffer{}
+			require.NoError(t, bundleFiles(ctx, c.filePath, zipped, ""))
+			out := &bytes.Buffer{}
+			assert.NoError(t, runBundled(ctx, zipped.Bytes(), out, ""))
+			assert.Equal(t, c.result+"\n", out.String())
+		})
+	}
+}
+
+func TestRunBundleWithoutRoot(t *testing.T) {
+	t.Parallel()
+	fs := afero.NewMemMapFs()
+	path := "very/deep/dir/test.arrai"
+	f, err := fs.Create(path)
+	require.NoError(t, err)
+	expectedOut := "1\n"
+	_, err = f.Write([]byte(expectedOut))
+	require.NoError(t, err)
+	f.Close()
+
+	ctx := ctxfs.SourceFsOnto(context.Background(), fs)
+	zipped := &bytes.Buffer{}
+	require.NoError(t, bundleFiles(ctx, path, zipped, ""))
+	out := &bytes.Buffer{}
+	assert.NoError(t, runBundled(ctx, zipped.Bytes(), out, ""))
+	assert.Equal(t, expectedOut, out.String())
+}
+
+func TestModuleImportRoot(t *testing.T) {
+	t.Parallel()
+
+	importTestFs := getImportFs(t)
 	ctx := ctxfs.SourceFsOnto(context.Background(), importTestFs)
 	cases := []struct {
 		filePath, expected string
 	}{
-		{"path1/path2/path3/import_from_root.arrai", "2"},
-		{"path1/path2/path4/import_from_same_dir_root.arrai", "1"},
-		{"path1/path2/path4/path5/import_from_nested_root.arrai", "1"},
+		{"/path1/path2/path3/import_from_root.arrai", "2"},
+		{"/path1/path2/path4/import_from_same_dir_root.arrai", "1"},
+		{"/path1/path2/path4/path5/import_from_nested_root.arrai", "1"},
 	}
 	for _, c := range cases {
 		var buf bytes.Buffer
-		require.NoError(t, evalFile(ctx, mustAbs(t, c.filePath), &buf, ""))
+		require.NoError(t, evalFile(ctx, c.filePath, &buf, ""))
 		require.Equal(t, c.expected+"\n", buf.String())
 	}
 }
