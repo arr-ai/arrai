9cf56cd95830ac1f58f5ff01aa200e6a04fb38de
Oliver Lade <oliver.lade@anz.com>
2020-09-16 09:51:52 +1000

    Add unary shell command test

--- syntax/std_deprecated_test.go
+++ syntax/std_deprecated_test.go
@@ -3,15 +3,25 @@ package syntax
 import "testing"
 
 func TestDeprecatedExec(t *testing.T) {
+	t.Parallel()
+
 	AssertCodesEvalToSameValue(t, `(
 		args: ['ls', 'std_deprecated_test.go'],
 		exitCode: 0,
 		stdout: <<'std_deprecated_test.go\n'>>,
 		stderr: {},
 	)`, `//deprecated.exec(['ls', 'std_deprecated_test.go'])`)
+	AssertCodesEvalToSameValue(t, `(
+		args: ['echo'],
+		exitCode: 0,
+		stdout: <<'\n'>>,
+		stderr: {},
+	)`, `//deprecated.exec(['echo'])`)
 }
 
 func TestDeprecatedExecError(t *testing.T) {
+	t.Parallel()
+
 	// TODO: Not sure why the stderr string looks the way it does.
 	AssertCodesEvalToSameValue(t, `(
 		args: ['ls', 'nonexistent'],
@@ -22,6 +32,8 @@ func TestDeprecatedExecError(t *testing.T) {
 }
 
 func TestDeprecatedExecFail(t *testing.T) {
+	t.Parallel()
+
 	AssertCodeErrors(t, ``, `//deprecated.exec('')`)
 	AssertCodeErrors(t, ``, `//deprecated.exec(['ls std_deprecated_test.go'])`)
 }
