1931aacc1e7511dc8bbae50f30fc8409e524c946
Oliver Lade <oliver.lade@anz.com>
2020-09-23 13:20:59 +1000

    Add error return for bad typing in //rel.union

--- syntax/std_rel_test.go
+++ syntax/std_rel_test.go
@@ -10,3 +10,10 @@ func TestRelUnion(t *testing.T) {
 	AssertCodesEvalToSameValue(t, `{1}         `, `//rel.union({{1}, {1}, {1}})         `)
 	AssertCodesEvalToSameValue(t, `{}          `, `//rel.union({})                      `)
 }
+
+func TestRelUnionError(t *testing.T) {
+	t.Parallel()
+
+	AssertCodeErrors(t, `arg to //rel.union must be set, not rel.GenericTuple`, `//rel.union(())`)
+	AssertCodeErrors(t, `elems of set arg to //rel.union must be sets, not *rel.GenericTuple`, `//rel.union({()})`)
+}
