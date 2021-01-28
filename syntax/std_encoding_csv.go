340e85cf8c25dc4f737c4a2f841a6d74d5dd9821
Andrew Emery <andrew.emery@anz.com>
2021-01-29 10:55:52 +1100

    Fixes problem encoding CSV with empty string
    This fix addresses an issue whereby attempting to encode a CSV that
    contained an empty string would fail.

--- syntax/std_encoding_csv.go
+++ syntax/std_encoding_csv.go
@@ -155,7 +155,11 @@ func csvEncodeFnBody(fn string, value rel.Value, config encodeConfig) (rel.Value
 		}
 		record := make([]string, rowArray.Count())
 		for j, value := range rowArray.Values() {
-			valueString, ok := value.(rel.String)
+			valueSet, ok := value.(rel.Set)
+			if !ok {
+				return nil, errors.Errorf("value %v of record %v must be string, not %v", j, i, rel.ValueTypeAsString(value))
+			}
+			valueString, ok := rel.AsString(valueSet)
 			if !ok {
 				return nil, errors.Errorf("value %v of record %v must be string, not %v", j, i, rel.ValueTypeAsString(value))
 			}
