979277500b4900c067c7d1db43189085e782e7ca
Ashwin Sajiv <sajivashwin@gmail.com>
2021-03-03 12:48:40 +1100

    Address feedbacks

--- syntax/pattern_set_test.go
+++ syntax/pattern_set_test.go
@@ -6,20 +6,20 @@ import (
 
 func TestSetPatternWithTupleSet(t *testing.T) {
 	t.Parallel()
-	AssertCodesEvalToSameValue(t, `1`, `let x={(y:1)}; cond x{ {(:y)}: y*y }`)
-	AssertCodesEvalToSameValue(t, `{}`, `let x={(y:1)}; cond x{ {(:a)}: y*y }`)
+	AssertCodesEvalToSameValue(t, `1`, `cond {(y:1)} { {(:y)}: y*y }`)
+	AssertCodesEvalToSameValue(t, `{}`, `cond {(y:1)}{ {(:a)}: y*y }`)
 
 	AssertCodesEvalToSameValue(t,
-		`4`, `let m=(a: {(x: 3)}, b: {(x: 3)}); cond m{ (a:{(:x)}, b: {(:x)}): x+1}`)
+		`4`, `let m=(a: {(x: 3)}, b: {(x: 3)}); cond m { (a:{(:x)}, b: {(:x)}): x+1}`)
 	AssertCodesEvalToSameValue(t,
-		`{}`, `let m=(a: {(x: 3)}, b: {(x: 3)}); cond m{ (a:{(:a)}, b: {(:x)}): x+1}`)
+		`{}`, `let m=(a: {(x: 3)}, b: {(x: 3)}); cond m { (a:{(:a)}, b: {(:x)}): x+1}`)
 
-	AssertCodesEvalToSameValue(t, `{(2)}`, `let x={(y:1, z:2)}; cond x{ {(:y, :z)}: {(y*z)} }`)
-	AssertCodesEvalToSameValue(t, `{}`, `let x={(y:1, z:2)}; cond x{ {(:a, :z)}: {(y*z)} }`)
+	AssertCodesEvalToSameValue(t, `{(2)}`, `cond {(y:1, z:2)} { {(:y, :z)}: {(y*z)} }`)
+	AssertCodesEvalToSameValue(t, `{}`, `cond {(y:1, z:2)} { {(:a, :z)}: {(y*z)} }`)
 
 	AssertCodesEvalToSameValue(t,
-		`2`, `let x={(y:0, z:2), (y:1, z:3)}; let w = x where .y=1; cond w{ {(:y, ...)}: 2*y }`)
-	AssertCodesEvalToSameValue(t, `{}`, `let x={(y:0, z:2), (y:0, z:3)}; cond x{ {(:y, :z)}: 2*y }`)
+		`2`, `let w = {(y:0, z:2), (y:1, z:3)} where .y=1; cond w { {(:y, ...)}: 2*y }`)
+	AssertCodesEvalToSameValue(t, `{}`, `cond {(y:0, z:2), (y:0, z:3)} { {(:y, :z)}: 2*y }`)
 
 	// TODO: This should be an error
 	AssertCodePanics(t, `let x = {(y:0, z:2), (y:0, z:3)}; cond x { {(:y, :z), ...}: 2*y }`)
