4eb531536e968aadeb2af7cefef3f8718c768f7f
nandakn1 <72120403+nandakn1@users.noreply.github.com>
2021-11-03 16:19:36 +1100

    strip whitespace from importPath

--- syntax/package_test.go
+++ syntax/package_test.go
@@ -38,7 +38,8 @@ func TestPackageImport(t *testing.T) {
 	t.Parallel()
 	AssertCodesEvalToSameValue(t, `{1, 4, 9, 16}`, `//{./examples/simple/simple}        `)
 	AssertCodesEvalToSameValue(t, `1            `, `//{/examples/import/relative_import}`)
-	AssertCodesEvalToSameValue(t, `2            `, `//{/examples/import/comb_import}    `)
+	AssertCodesEvalToSameValue(t, `2            `, `//{/examples/import/comb_import}   `)
+	AssertCodesEvalToSameValue(t, `2            `, `//{    /examples/import/comb_import    }   `)
 }
 
 func TestPackageImportFromRoot(t *testing.T) {
